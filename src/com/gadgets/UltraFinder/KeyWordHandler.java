a8253c3d8d8a7342d6ca845b9a43be1dc6962607
Janux <jackyshu1107@gmail.com>
2019-02-06 21:20:15 +0800

    implement matcher

--- src/com/gadgets/UltraFinder/KeyWordHandler.java
+++ src/com/gadgets/UltraFinder/KeyWordHandler.java
@@ -1,16 +1,28 @@
 package com.gadgets.UltraFinder;
 
 import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class KeyWordHandler {
 
-	public KeyWordHandler() {
-		HashSet<String> keywordPatterns = new HashSet<>();
+	HashSet<String> keywordPatterns;
+
+	public KeyWordHandler(HashSet<String> keywordPatterns) {
+		this.keywordPatterns = keywordPatterns;
 
 	}
 
 	public boolean checkKeyWordInLine(String line) {
+		for (String pattern_str : keywordPatterns) {
+			Pattern keywordPatter = Pattern.compile(pattern_str);
 
+			Matcher matcher = keywordPatter.matcher(line);
+			if (matcher.matches()) {
+				return true;
+			}
+
+		}
 		return false;
 
 	}
